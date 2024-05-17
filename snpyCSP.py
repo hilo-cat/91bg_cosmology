7d3897bd38c33b7a314a5686f95e9aa30f83debf
Mekhi Woods <mekhidw@hawaii.edu>
2024-05-17 13:57:34 -1000

    updates

--- snpyCSP.py
+++ snpyCSP.py
@@ -1,14 +1,31 @@
 import numpy as np 
 import snpy
 
-def temp():
-    print('hi')
+TEST_PATH = 'data\\CSP\\SN2005bl_snpy.txt'
 
+
+def SNooPy_fit():
+    # Load data
+    s = snpy.get_sn(TEST_PATH)
+    
+    # Set model
+    model = 'max_model'
+    shapeParam = 'st'
+    s.choose_model(model, stype=shapeParam)
+    s.set_restbands() # Auto-sets appropriate rest bands
+
+    # Set fit
+    s.fit()
+
+    # Summary
+    s.summary()
+    for param in s.paramaters:
+        print("{} = {} +/- {}".format(param, s.parameters[param], s.errors[param]))
 
     return None
 
 if __name__ == "__main__":
     
-    temp()
+    SNooPy_fit()
 
 
